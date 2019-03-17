a19402457a1bea0fc604b1016071c63088291e9b
Maliha Islam <maliha.islam@points.com>
2019-03-17 01:43:28 -0400

    add home page

---
+++ src/constants.js
@@ -0,0 +1,607 @@
+export const BASE_URL = 'http://localhost:3000/';
+
+export const CHARITY_CATEGORY_CODE_MAP = {
+  1: {
+    'charityTypeEn': 'Welfare',
+    'charityTypeFr': 'Bien-être',
+    'descEn': 'Organizations Providing Care Other than Treatment',
+    'descFr': 'Organisme fournissant des soins autres que des traitements'
+  },
+  2: {
+    'charityTypeEn': 'Welfare',
+    'charityTypeFr': 'Bien-être',
+    'descEn': 'Disaster Funds',
+    'descFr': 'Fonds pour les désastres'
+  },
+  3: {
+    'charityTypeEn': 'Welfare',
+    'charityTypeFr': 'Bien-être',
+    'descEn': '(Welfare) Charitable Corporations',
+    'descFr': '(Bien-être) Corporations de bienfaisance'
+  },
+  5: {
+    'charityTypeEn': 'Welfare',
+    'charityTypeFr': 'Bien-être',
+    'descEn': '(Welfare) Charitable Trusts',
+    'descFr': '(Bien-être) Fiducies de bienfaisance'
+  },
+  9: {
+    'charityTypeEn': 'Welfare',
+    'charityTypeFr': 'Bien-être',
+    'descEn': 'Welfare Organizations (not else classified)',
+    'descFr': 'Organisme de bien-être, (sans autre classification)'
+  },
+  10: {
+    'charityTypeEn': 'Health',
+    'charityTypeFr': 'Santé',
+    'descEn': 'Hospitals',
+    'descFr': 'Hôpitaux'
+  },
+  11: {
+    'charityTypeEn': 'Health',
+    'charityTypeFr': 'Santé',
+    'descEn': 'Services Other Than Hospitals Services',
+    'descFr': 'Hôpitaux'
+  },
+  13: {
+    'charityTypeEn': 'Health',
+    'charityTypeFr': 'Santé',
+    'descEn': '(Health) Charitable Corporations',
+    'descFr': '(Santé) Corporations de Bienfaisance'
+  },
+  15: {
+    'charityTypeEn': 'Health',
+    'charityTypeFr': 'Santé',
+    'descEn': '(Health) Charitable Trusts',
+    'descFr': '(Santé) Fiducies de bienfaisance'
+  },
+  19: {
+    'charityTypeEn': 'Health',
+    'charityTypeFr': 'Santé',
+    'descEn': 'Health Organizations, (not else classified)',
+    'descFr': 'Organismes de santé, (sans autre classification)'
+  },
+  20: {
+    'charityTypeEn': 'Education',
+    'charityTypeFr': 'Éducatifs',
+    'descEn': 'Teaching Institutions or Institutions of Learning',
+    'descFr': 'Institutions d’enseignement ou institutions d’apprentissage'
+  },
+  21: {
+    'charityTypeEn': 'Education',
+    'charityTypeFr': 'Éducatifs',
+    'descEn': 'Support of Schools and Education',
+    'descFr': 'Appuie aux écoles et à l’éducation'
+  },
+  22: {
+    'charityTypeEn': 'Education',
+    'charityTypeFr': 'Éducatifs',
+    'descEn': 'Cultural Activities and Promotion of the Arts',
+    'descFr': 'Activités culturelles et promotion des arts'
+  },
+  23: {
+    'charityTypeEn': 'Education',
+    'charityTypeFr': 'Éducatifs',
+    'descEn': '(Education) Charitable Corporations',
+    'descFr': '(Éducation) Corporations de bienfaisance'
+  },
+  25: {
+    'charityTypeEn': 'Education',
+    'charityTypeFr': 'Éducatifs',
+    'descEn': '(Education) Charitable Trusts',
+    'descFr': '(Éducation) Fiducies de bienfaisance'
+  },
+  29: {
+    'charityTypeEn': 'Education',
+    'charityTypeFr': 'Éducatifs',
+    'descEn': 'Education Organizations, (not else classified)',
+    'descFr': 'Organismes éducatifs, (sans autre classification)'
+  },
+  30: {
+    'charityTypeEn': 'Religion',
+    'charityTypeFr': 'Religion',
+    'descEn': 'Anglican Parishes',
+    'descFr': 'Paroisses anglicanes'
+  },
+  31: {
+    'charityTypeEn': 'Religion',
+    'charityTypeFr': 'Religion',
+    'descEn': 'Baptist Congregations',
+    'descFr': 'Congrégations baptistes'
+  },
+  32: {
+    'charityTypeEn': 'Religion',
+    'charityTypeFr': 'Religion',
+    'descEn': 'Lutheran Congregations',
+    'descFr': 'Congrégations luthériennes'
+  },
+  34: {
+    'charityTypeEn': 'Religion',
+    'charityTypeFr': 'Religion',
+    'descEn': 'Mennonite Congregations',
+    'descFr': 'Congrégations mennonites'
+  },
+  36: {
+    'charityTypeEn': 'Religion',
+    'charityTypeFr': 'Religion',
+    'descEn': 'Pentecostal Assemblies (Pentecostal Assemblies) of Canada only',
+    'descFr': 'Assemblées de la Pentecôte seulement'
+  },
+  37: {
+    'charityTypeEn': 'Religion',
+    'charityTypeFr': 'Religion',
+    'descEn': 'Presbyterian Congregations',
+    'descFr': 'Congrégations presbytériennes'
+  },
+  38: {
+    'charityTypeEn': 'Religion',
+    'charityTypeFr': 'Religion',
+    'descEn': 'Roman Catholic Parishes and Chapels',
+    'descFr': 'Paroisses catholiques romaines et chapelles'
+  },
+  39: {
+    'charityTypeEn': 'Religion',
+    'charityTypeFr': 'Religion',
+    'descEn': 'Other Denominations’ Congregations or Parishes, (not else classified)',
+    'descFr': 'Autres dénominations, congrégations ou paroisses, (sans autre classification)'
+  },
+  40: {
+    'charityTypeEn': 'Religion',
+    'charityTypeFr': 'Religion',
+    'descEn': 'Salvation Army Temples',
+    'descFr': 'Temples de l’armée du Salut'
+  },
+  41: {
+    'charityTypeEn': 'Religion',
+    'charityTypeFr': 'Religion',
+    'descEn': 'Seventh Day Adventist Congregations',
+    'descFr': 'Congrégations Adventiste du Septième Jour'
+  },
+  42: {
+    'charityTypeEn': 'Religion',
+    'charityTypeFr': 'Religion',
+    'descEn': 'Synagogues',
+    'descFr': 'Synagogues'
+  },
+  43: {
+    'charityTypeEn': 'Religion',
+    'charityTypeFr': 'Religion',
+    'descEn': '(Religion) Charitable Organizations',
+    'descFr': '(Religion) Corporations de bienfaisance'
+  },
+  44: {
+    'charityTypeEn': 'Religion',
+    'charityTypeFr': 'Religion',
+    'descEn': 'United Church Congregations',
+    'descFr': 'Congrégation de l’église uni'
+  },
+  45: {
+    'charityTypeEn': 'Religion',
+    'charityTypeFr': 'Religion',
+    'descEn': '(Religion) Charitable Trusts',
+    'descFr': '(Religion) Fiducies de bienfaisance'
+  },
+  46: {
+    'charityTypeEn': 'Religion',
+    'charityTypeFr': 'Religion',
+    'descEn': 'Convents and Monasteries',
+    'descFr': 'Couvents et monastères'
+  },
+  47: {
+    'charityTypeEn': 'Religion',
+    'charityTypeFr': 'Religion',
+    'descEn': 'Missionary Organizations and Propagation of Gospel',
+    'descFr': 'Organismes missionnaires et propagation de l’Évangile'
+  },
+  49: {
+    'charityTypeEn': 'Religion',
+    'charityTypeFr': 'Religion',
+    'descEn': 'Religious Organizations, (not else classified)',
+    'descFr': 'Organismes religieux, (sans autre classification)'
+  },
+  50: {
+    'charityTypeEn': 'Benefits to Community',
+    'charityTypeFr': 'Communautaire',
+    'descEn': 'Libraries, Museums and Other Repositories',
+    'descFr': 'Bibliothèques, musées et autres collections'
+  },
+  51: {
+    'charityTypeEn': 'Benefits to Community',
+    'charityTypeFr': 'Communautaire',
+    'descEn': 'Military Units',
+    'descFr': 'Unités militaires'
+  },
+  52: {
+    'charityTypeEn': 'Benefits to Community',
+    'charityTypeFr': 'Communautaire',
+    'descEn': 'Preservation of Sites, Beauty and Historical',
+    'descFr': 'Préservation des sites pittoresques et historiques'
+  },
+  53: {
+    'charityTypeEn': 'Benefits to Community',
+    'charityTypeFr': 'Communautaire',
+    'descEn': '(Community) Charitable Corporations',
+    'descFr': '(Communauté) Corporations de bienfaisance'
+  },
+  54: {
+    'charityTypeEn': 'Benefits to Community',
+    'charityTypeFr': 'Communautaire',
+    'descEn': 'Protection of Animals',
+    'descFr': 'Protection des animaux'
+  },
+  55: {
+    'charityTypeEn': 'Benefits to Community',
+    'charityTypeFr': 'Communautaire',
+    'descEn': '(Community) Charitable Trusts (Other than Service Clubs and Fraternal Societies Projects)',
+    'descFr': '(Communauté) Fiducies de bienfaisance'
+  },
+  56: {
+    'charityTypeEn': 'Benefits to Community',
+    'charityTypeFr': 'Communautaire',
+    'descEn': 'Recreation, Playgrounds and Vacation Camps',
+    'descFr': 'Récréation, terrains de jeux et camps de vacances'
+  },
+  57: {
+    'charityTypeEn': 'Benefits to Community',
+    'charityTypeFr': 'Communautaire',
+    'descEn': 'Temperance Associations',
+    'descFr': 'Associations de tempérance'
+  },
+  59: {
+    'charityTypeEn': 'Benefits to Community',
+    'charityTypeFr': 'Communautaire',
+    'descEn': 'Community Organizations, (not else classified)',
+    'descFr': 'Organismes Communautaires, (sans autre classification)'
+  },
+  63: {
+    'charityTypeEn': 'Other',
+    'charityTypeFr': 'Autre',
+    'descEn': 'Service Clubs and Fraternal Societies’ Charitable Corporations',
+    'descFr': 'Corporations de bienfaisance de clubs de service'
+  },
+  65: {
+    'charityTypeEn': 'Other',
+    'charityTypeFr': 'Autre',
+    'descEn': 'Service Clubs and Fraternal Societies’ Projects',
+    'descFr': 'Projets de clubs de service et de société fraternelle'
+  },
+  75: {
+    'charityTypeEn': 'Other',
+    'charityTypeFr': 'Autre',
+    'descEn': 'Employees’ Charity Trusts',
+    'descFr': 'Fiducies de bienfaisance d’employés'
+  },
+  80: {
+    'charityTypeEn': 'Other',
+    'charityTypeFr': 'Autre',
+    'descEn': 'Registered Canadian Amateur Athletic Association (RCAAA)',
+    'descFr': 'Association Canadienne de sport amateur (ACESA)'
+  },
+  81: {
+    'charityTypeEn': 'Other',
+    'charityTypeFr': 'Autre',
+    'descEn': 'Registered National Arts Services Organization (RNASO)',
+    'descFr': 'Organisme enregistré de services nationaux dans le'
+  },
+  83: {
+    'charityTypeEn': 'Other',
+    'charityTypeFr': 'Autre',
+    'descEn': 'Corporation Funding Registered Canadian Amateur Athletic Association',
+    'descFr': 'Financement de l’ACESA inc'
+  },
+  85: {
+    'charityTypeEn': 'Other',
+    'charityTypeFr': 'Autre',
+    'descEn': 'Trust Funding Registered Canadian Amateur Athletic Association',
+    'descFr': 'Financement de l’ACESA par fiducies'
+  },
+  99: {
+    'charityTypeEn': 'Other',
+    'charityTypeFr': 'Autre',
+    'descEn': 'Miscellaneous Charitable Organizations, (not else classified)',
+    'descFr': 'Organismes de bienfaisance divers, (sans autre classification)'
+  }
+};
+
+export const CHARITY_CATEGORY_CODE_LIST = [
+  {
+    'charityTypeEn': 'Welfare',
+    'charityTypeFr': 'Bien-être',
+    'descEn': 'Organizations Providing Care Other than Treatment',
+    'descFr': 'Organisme fournissant des soins autres que des traitements'
+  },
+  {
+    'charityTypeEn': 'Welfare',
+    'charityTypeFr': 'Bien-être',
+    'descEn': 'Disaster Funds',
+    'descFr': 'Fonds pour les désastres'
+  },
+  {
+    'charityTypeEn': 'Welfare',
+    'charityTypeFr': 'Bien-être',
+    'descEn': '(Welfare) Charitable Corporations',
+    'descFr': '(Bien-être) Corporations de bienfaisance'
+  },
+  {
+    'charityTypeEn': 'Welfare',
+    'charityTypeFr': 'Bien-être',
+    'descEn': '(Welfare) Charitable Trusts',
+    'descFr': '(Bien-être) Fiducies de bienfaisance'
+  },
+  {
+    'charityTypeEn': 'Welfare',
+    'charityTypeFr': 'Bien-être',
+    'descEn': 'Welfare Organizations (not else classified)',
+    'descFr': 'Organisme de bien-être, (sans autre classification)'
+  },
+  {
+    'charityTypeEn': 'Health',
+    'charityTypeFr': 'Santé',
+    'descEn': 'Hospitals',
+    'descFr': 'Hôpitaux'
+  },
+  {
+    'charityTypeEn': 'Health',
+    'charityTypeFr': 'Santé',
+    'descEn': 'Services Other Than Hospitals Services',
+    'descFr': 'Hôpitaux'
+  },
+  {
+    'charityTypeEn': 'Health',
+    'charityTypeFr': 'Santé',
+    'descEn': '(Health) Charitable Corporations',
+    'descFr': '(Santé) Corporations de Bienfaisance'
+  },
+  {
+    'charityTypeEn': 'Health',
+    'charityTypeFr': 'Santé',
+    'descEn': '(Health) Charitable Trusts',
+    'descFr': '(Santé) Fiducies de bienfaisance'
+  },
+  {
+    'charityTypeEn': 'Health',
+    'charityTypeFr': 'Santé',
+    'descEn': 'Health Organizations, (not else classified)',
+    'descFr': 'Organismes de santé, (sans autre classification)'
+  },
+  {
+    'charityTypeEn': 'Education',
+    'charityTypeFr': 'Éducatifs',
+    'descEn': 'Teaching Institutions or Institutions of Learning',
+    'descFr': 'Institutions d’enseignement ou institutions d’apprentissage'
+  },
+  {
+    'charityTypeEn': 'Education',
+    'charityTypeFr': 'Éducatifs',
+    'descEn': 'Support of Schools and Education',
+    'descFr': 'Appuie aux écoles et à l’éducation'
+  },
+  {
+    'charityTypeEn': 'Education',
+    'charityTypeFr': 'Éducatifs',
+    'descEn': 'Cultural Activities and Promotion of the Arts',
+    'descFr': 'Activités culturelles et promotion des arts'
+  },
+  {
+    'charityTypeEn': 'Education',
+    'charityTypeFr': 'Éducatifs',
+    'descEn': '(Education) Charitable Corporations',
+    'descFr': '(Éducation) Corporations de bienfaisance'
+  },
+  {
+    'charityTypeEn': 'Education',
+    'charityTypeFr': 'Éducatifs',
+    'descEn': '(Education) Charitable Trusts',
+    'descFr': '(Éducation) Fiducies de bienfaisance'
+  },
+  {
+    'charityTypeEn': 'Education',
+    'charityTypeFr': 'Éducatifs',
+    'descEn': 'Education Organizations, (not else classified)',
+    'descFr': 'Organismes éducatifs, (sans autre classification)'
+  },
+  {
+    'charityTypeEn': 'Religion',
+    'charityTypeFr': 'Religion',
+    'descEn': 'Anglican Parishes',
+    'descFr': 'Paroisses anglicanes'
+  },
+  {
+    'charityTypeEn': 'Religion',
+    'charityTypeFr': 'Religion',
+    'descEn': 'Baptist Congregations',
+    'descFr': 'Congrégations baptistes'
+  },
+  {
+    'charityTypeEn': 'Religion',
+    'charityTypeFr': 'Religion',
+    'descEn': 'Lutheran Congregations',
+    'descFr': 'Congrégations luthériennes'
+  },
+  {
+    'charityTypeEn': 'Religion',
+    'charityTypeFr': 'Religion',
+    'descEn': 'Mennonite Congregations',
+    'descFr': 'Congrégations mennonites'
+  },
+  {
+    'charityTypeEn': 'Religion',
+    'charityTypeFr': 'Religion',
+    'descEn': 'Pentecostal Assemblies (Pentecostal Assemblies) of Canada only',
+    'descFr': 'Assemblées de la Pentecôte seulement'
+  },
+  {
+    'charityTypeEn': 'Religion',
+    'charityTypeFr': 'Religion',
+    'descEn': 'Presbyterian Congregations',
+    'descFr': 'Congrégations presbytériennes'
+  },
+  {
+    'charityTypeEn': 'Religion',
+    'charityTypeFr': 'Religion',
+    'descEn': 'Roman Catholic Parishes and Chapels',
+    'descFr': 'Paroisses catholiques romaines et chapelles'
+  },
+  {
+    'charityTypeEn': 'Religion',
+    'charityTypeFr': 'Religion',
+    'descEn': 'Other Denominations’ Congregations or Parishes, (not else classified)',
+    'descFr': 'Autres dénominations, congrégations ou paroisses, (sans autre classification)'
+  },
+  {
+    'charityTypeEn': 'Religion',
+    'charityTypeFr': 'Religion',
+    'descEn': 'Salvation Army Temples',
+    'descFr': 'Temples de l’armée du Salut'
+  },
+  {
+    'charityTypeEn': 'Religion',
+    'charityTypeFr': 'Religion',
+    'descEn': 'Seventh Day Adventist Congregations',
+    'descFr': 'Congrégations Adventiste du Septième Jour'
+  },
+  {
+    'charityTypeEn': 'Religion',
+    'charityTypeFr': 'Religion',
+    'descEn': 'Synagogues',
+    'descFr': 'Synagogues'
+  },
+  {
+    'charityTypeEn': 'Religion',
+    'charityTypeFr': 'Religion',
+    'descEn': '(Religion) Charitable Organizations',
+    'descFr': '(Religion) Corporations de bienfaisance'
+  },
+  {
+    'charityTypeEn': 'Religion',
+    'charityTypeFr': 'Religion',
+    'descEn': 'United Church Congregations',
+    'descFr': 'Congrégation de l’église uni'
+  },
+  {
+    'charityTypeEn': 'Religion',
+    'charityTypeFr': 'Religion',
+    'descEn': '(Religion) Charitable Trusts',
+    'descFr': '(Religion) Fiducies de bienfaisance'
+  },
+  {
+    'charityTypeEn': 'Religion',
+    'charityTypeFr': 'Religion',
+    'descEn': 'Convents and Monasteries',
+    'descFr': 'Couvents et monastères'
+  },
+  {
+    'charityTypeEn': 'Religion',
+    'charityTypeFr': 'Religion',
+    'descEn': 'Missionary Organizations and Propagation of Gospel',
+    'descFr': 'Organismes missionnaires et propagation de l’Évangile'
+  },
+  {
+    'charityTypeEn': 'Religion',
+    'charityTypeFr': 'Religion',
+    'descEn': 'Religious Organizations, (not else classified)',
+    'descFr': 'Organismes religieux, (sans autre classification)'
+  },
+  {
+    'charityTypeEn': 'Benefits to Community',
+    'charityTypeFr': 'Communautaire',
+    'descEn': 'Libraries, Museums and Other Repositories',
+    'descFr': 'Bibliothèques, musées et autres collections'
+  },
+  {
+    'charityTypeEn': 'Benefits to Community',
+    'charityTypeFr': 'Communautaire',
+    'descEn': 'Military Units',
+    'descFr': 'Unités militaires'
+  },
+  {
+    'charityTypeEn': 'Benefits to Community',
+    'charityTypeFr': 'Communautaire',
+    'descEn': 'Preservation of Sites, Beauty and Historical',
+    'descFr': 'Préservation des sites pittoresques et historiques'
+  },
+  {
+    'charityTypeEn': 'Benefits to Community',
+    'charityTypeFr': 'Communautaire',
+    'descEn': '(Community) Charitable Corporations',
+    'descFr': '(Communauté) Corporations de bienfaisance'
+  },
+  {
+    'charityTypeEn': 'Benefits to Community',
+    'charityTypeFr': 'Communautaire',
+    'descEn': 'Protection of Animals',
+    'descFr': 'Protection des animaux'
+  },
+  {
+    'charityTypeEn': 'Benefits to Community',
+    'charityTypeFr': 'Communautaire',
+    'descEn': '(Community) Charitable Trusts (Other than Service Clubs and Fraternal Societies Projects)',
+    'descFr': '(Communauté) Fiducies de bienfaisance'
+  },
+  {
+    'charityTypeEn': 'Benefits to Community',
+    'charityTypeFr': 'Communautaire',
+    'descEn': 'Recreation, Playgrounds and Vacation Camps',
+    'descFr': 'Récréation, terrains de jeux et camps de vacances'
+  },
+  {
+    'charityTypeEn': 'Benefits to Community',
+    'charityTypeFr': 'Communautaire',
+    'descEn': 'Temperance Associations',
+    'descFr': 'Associations de tempérance'
+  },
+  {
+    'charityTypeEn': 'Benefits to Community',
+    'charityTypeFr': 'Communautaire',
+    'descEn': 'Community Organizations, (not else classified)',
+    'descFr': 'Organismes Communautaires, (sans autre classification)'
+  },
+  {
+    'charityTypeEn': 'Other',
+    'charityTypeFr': 'Autre',
+    'descEn': 'Service Clubs and Fraternal Societies’ Charitable Corporations',
+    'descFr': 'Corporations de bienfaisance de clubs de service'
+  },
+  {
+    'charityTypeEn': 'Other',
+    'charityTypeFr': 'Autre',
+    'descEn': 'Service Clubs and Fraternal Societies’ Projects',
+    'descFr': 'Projets de clubs de service et de société fraternelle'
+  },
+  {
+    'charityTypeEn': 'Other',
+    'charityTypeFr': 'Autre',
+    'descEn': 'Employees’ Charity Trusts',
+    'descFr': 'Fiducies de bienfaisance d’employés'
+  },
+  {
+    'charityTypeEn': 'Other',
+    'charityTypeFr': 'Autre',
+    'descEn': 'Registered Canadian Amateur Athletic Association (RCAAA)',
+    'descFr': 'Association Canadienne de sport amateur (ACESA)'
+  },
+  {
+    'charityTypeEn': 'Other',
+    'charityTypeFr': 'Autre',
+    'descEn': 'Registered National Arts Services Organization (RNASO)',
+    'descFr': 'Organisme enregistré de services nationaux dans le'
+  },
+  {
+    'charityTypeEn': 'Other',
+    'charityTypeFr': 'Autre',
+    'descEn': 'Corporation Funding Registered Canadian Amateur Athletic Association',
+    'descFr': 'Financement de l’ACESA inc'
+  },
+  {
+    'charityTypeEn': 'Other',
+    'charityTypeFr': 'Autre',
+    'descEn': 'Trust Funding Registered Canadian Amateur Athletic Association',
+    'descFr': 'Financement de l’ACESA par fiducies'
+  },
+  {
+    'charityTypeEn': 'Other',
+    'charityTypeFr': 'Autre',
+    'descEn': 'Miscellaneous Charitable Organizations, (not else classified)',
+    'descFr': 'Organismes de bienfaisance divers, (sans autre classification)'
+  }
+];
---
+++ src/react-components/category-card-list/index.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import kebabCase from 'lodash-es/kebabCase';
+
+import {BaseReactComponent} from "../base-react-component";
+import CategoryCard from '../category-card';
+import {CHARITY_CATEGORY_CODE_LIST} from "../../constants";
+
+import './style.css';
+
+export class CategoryCardList extends BaseReactComponent {
+    filterState() {
+        return {};
+    }
+
+    render() {
+        const classes = {
+            header: {
+                color: 'red'
+            }
+        };
+        const getCategoryItems = (category) => (
+            CHARITY_CATEGORY_CODE_LIST.filter(char => char['charityTypeEn'] === category).map(
+                item => (
+                    <li className="category-card-list__item" key={kebabCase(item.descEn)}>
+                        <CategoryCard classes={classes} title={item.descEn} />
+                    </li>
+                ))
+        );
+
+        const categories = ['Welfare', 'Health', 'Education', 'Religion', 'Benefits to Community', 'Other'].map(
+            category => (
+                <div>
+                    <h2 className="category-card-list__header">{category}</h2>
+                    <img className="category-card-list__image" src={`/images/${kebabCase(category)}.png`} alt={category} />
+                    <ul className="category-card-list__list">
+                        {getCategoryItems(category)}
+                    </ul>
+                    <br/><br/><br/>
+                </div>
+            ));
+
+        return (
+            <div className="category-card-list">
+                {categories}
+            </div>
+        );
+    }
+}
